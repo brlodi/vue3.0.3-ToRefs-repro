46dd2dcc593421b5fa033fde5f238aa128c0a1ad
Benjamin Lodi <brlodi@gmail.com>
2020-11-29 17:10:53 -0500

    Minimal working configuration under Vue@3.0.2

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div :style="{ color: displayStyle.fontColor.value, fontSize: displayStyle.fontSize.value }">
+    <strong>App state: "{{ data.state.value }}"</strong>
+    <br>
+    <button @click="cycleState">Cycle State</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { computed, defineComponent, PropType, reactive, Ref, toRefs, ToRefs } from 'vue';
+
+type FontSize = number | string;
+type FontColor = 'blue' | 'red' | 'green';
+
+interface AppProps {
+  fontSize: FontSize;
+  fontColor: FontColor;
+}
+
+interface AppData {
+  state: 'state1' | 'state2' | 'state3';
+}
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  props: {
+    fontSize: [Number, String] as PropType<FontSize>,
+    fontColor: String as PropType<FontColor>,
+  },
+  setup(props: Partial<AppProps>) {
+    const data: ToRefs<AppData> = toRefs(reactive({
+      state: 'state1',
+    }));
+
+    const cycleState = () => {
+      switch (data.state.value) {
+        case 'state1':
+          data.state.value = 'state2';
+          break;
+        case 'state2':
+          data.state.value = 'state3';
+          break;
+        case 'state3':
+          data.state.value = 'state1';
+          break;
+      }
+    };
+
+    const displayStyle: ToRefs<AppProps> = {
+      fontSize: computed<FontSize>(() => props.fontSize ?? '24px'),
+      fontColor: computed<FontColor>(() => props.fontColor ?? 'blue')
+    };
+
+    return {
+      data,
+      displayStyle,
+      cycleState,
+    };
+  },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
